67babbf26e1603fd66012813bc15cab7092db4a0
Adhmir Gomes <55671798+Adhmir@users.noreply.github.com>
2026-04-29 14:08:53 -0300

    Refactor and update ESG valuation logic

--- pages/5_Resultados.py
+++ pages/5_Resultados.py
@@ -9,7 +9,7 @@ st.set_page_config(page_title="Resultados ESG (FCFF)", layout="centered")
 # =========================
 # PARÂMETROS DA REGRESSÃO
 # =========================
-THETA_MEAN = 0,01454135691803
+THETA_MEAN = 0.01454135691803
 THETA_LOW = -0.0015497
 THETA_HIGH = 0.0127738
    
